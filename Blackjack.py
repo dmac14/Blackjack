2c210337fe4a727f00d28dfddff5826b66e49902
dmac14 <choosemcd14@gmail.com>
2014-09-21 09:52:00 -0700

    Version 1.1
    Made it so the cards were actually dealt to the player’s hand (whoops)
    and began the text interface for playing the game

--- Blackjack.py
+++ Blackjack.py
@@ -20,15 +20,16 @@ class Deck():
     #Choses a number and type for the card to be dealt. Checks if it has already
     #been dealt (if yes, calls method again). If not, deals it and adds it to
     #list of dealt cards.
-    def dealCard(self):
+    def dealCard(self,hand):
         self.randCardNum=random.randint(0,12)
         self.randCardType=random.randint(0,3)
         self.dealtCard=(str(self.cardNum[self.randCardNum]),self.cardType[self.randCardType],)
         for card in self.dealtCards:
             if self.dealtCard==card:
                 print("match")
-                return self.dealCard()
+                return self.dealCard(hand)
         self.dealtCards.append(self.dealtCard)
+        hand.heldCards.append(self.dealtCard)
         return self.dealtCard
 
 class PlayerHand():
@@ -36,9 +37,18 @@ class PlayerHand():
         self.heldCards=[]
 
 
+print("Welcome to our humble Blackjack table! Have a seat and we'll get you started :D")
+
+#Create a Deck and PlayerHand objects
 myDeck=Deck()
 myHand=PlayerHand()
-myDeck.dealCard()
+
+#Deal two cards to the player
+print("Alright then, here are your first two cards.")
+myDeck.dealCard(myHand)
+myDeck.dealCard(myHand)
+for card in myHand.heldCards:
+    print(card[0]+" of "+card[1])
 
 
 
